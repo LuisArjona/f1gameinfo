35d821d02a346dc5ee913fb4b5531bfdd8da1b2a
LuisArjona <luisarjonaaguilar@gmail.com>
2025-03-18 14:07:03 +0100

    Funcionalidad de comprar cartas

--- frontend/front-f1gameinfo/src/app/comprarcartas/comprarcartas.component.ts
+++ frontend/front-f1gameinfo/src/app/comprarcartas/comprarcartas.component.ts
@@ -1,11 +1,88 @@
 import { Component } from '@angular/core';
+import { CartaService } from '../carta.service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-comprarcartas',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './comprarcartas.component.html',
-  styleUrl: './comprarcartas.component.css'
+  styleUrl: './comprarcartas.component.css',
 })
 export class ComprarcartasComponent {
+  pilotos: any[] = [];
+  circuitos: any[] = [];
+  pilotosUsu: any[] = [];
+  circuitosUsu: any[] = [];
+  monedas: number = 0;
 
+  constructor(private cartaService: CartaService) {}
+
+  ngOnInit() {
+    this.cartaService.getPilotos().subscribe((data) => {
+      this.pilotos = data;
+    });
+
+    this.cartaService.getCircuitos().subscribe((data) => {
+      this.circuitos = data;
+    });
+
+    this.cartaService.getMonedasFromUsuario().subscribe((data) => {
+      this.monedas = data;
+    });
+
+    this.cartaService.getCartasCompradas().subscribe((data) => {
+      this.pilotosUsu = Array.from(data.pilotos);
+      this.circuitosUsu = Array.from(data.circuitos);
+    });
+  }
+
+  dividirEnFilas(array: any[], tamanoFila: number): any[][] {
+    const filas: any[][] = [];
+    for (let i = 0; i < array.length; i += tamanoFila) {
+      filas.push(array.slice(i, i + tamanoFila));
+    }
+    return filas;
+  }
+
+  comprarPiloto(piloto: any) {
+    if (this.monedas >= piloto.precio) {
+      this.cartaService.actualizarCartasUsuario(this.monedas - piloto.precio, piloto.id).subscribe(() => {
+        this.pilotosUsu.push(piloto);
+        this.monedas -= piloto.precio;
+      });
+    }
+  }
+
+  comprarCircuito(circuito: any) {
+    if (this.monedas >= circuito.precio) {
+      this.cartaService.actualizarCartasUsuario(this.monedas - circuito.precio, undefined, circuito.id).subscribe(() => {
+        this.circuitosUsu.push(circuito);
+        this.monedas -= circuito.precio;
+      });
+    }
+  }
+
+  isPilotoDisabled(piloto: any): boolean {
+    return this.monedas < piloto.precio || this.pilotosUsu.some(p => p.id === piloto.id);
+  }
+  
+  isCircuitoDisabled(circuito: any): boolean {
+    return this.monedas < circuito.precio || this.circuitosUsu.some(c => c.id === circuito.id);
+  }
+
+  isCircuitoComprado(circuito: any): boolean {
+    return this.circuitosUsu.some(c => c.id === circuito.id);
+  }
+
+  isPilotoComprado(piloto: any): boolean {
+    return this.pilotosUsu.some(p => p.id === piloto.id);
+  }
+
+  incrementarMonedas() {
+    this.cartaService.actualizarCartasUsuario(this.monedas + 100).subscribe(() => {
+        this.monedas += 100;
+    });
+  }
+  
 }
