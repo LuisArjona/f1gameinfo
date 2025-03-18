7e8e4c72af5fe97fcb20dd743ce1b1e483f1d54a
LuisArjona <luisarjonaaguilar@gmail.com>
2025-03-18 17:04:02 +0100

    Remodelar el login e incluir sweetalert

--- frontend/front-f1gameinfo/src/app/loguear/loguear.component.ts
+++ frontend/front-f1gameinfo/src/app/loguear/loguear.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { UsuarioServicioService } from '../usuario-servicio.service';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-loguear',
@@ -15,8 +16,14 @@ export class LoguearComponent {
   email: string = "";
   exito: boolean = false;
   intentado: boolean = false;
+  showPassword: boolean = false;
+
 
   constructor(private usuService: UsuarioServicioService, private router: Router) {}
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 
   loguearUsuario() {
     this.usuService.loguearUsuarioDos(this.email, this.pass).subscribe({
@@ -26,9 +33,11 @@ export class LoguearComponent {
           localStorage.setItem("token", JSON.stringify(token));
           this.exito = true;
           this.router.navigate(["/info"]);
+          Swal.fire({title: "Bienvenido", text: "Disfruta de F1GameInfo", icon: "success"});
         }
       },
       error: (error) => {
+        Swal.fire({title: "Algo ha ido mal", text: "El usuario o la contraseña no son correctos", icon: "error"});
         this.exito = false;
       }
     });
